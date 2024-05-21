09871e0ee82c8aa88a57a0121021e43912f5d822
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-05-21 19:10:44 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angularLesDirectives';
+  isAdmin = true;
+  changeAdmin() {
+    this.isAdmin = !this.isAdmin
+  }
 }
